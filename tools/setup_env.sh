d8ef31cd956c3001afc687c008fb3d628989e6fe
Turra <joao.t06@hotmail.com>
2024-10-24 23:48:40 -0300

    Add initial alternative PSO to ROS simulation

--- tools/setup_env.sh
+++ tools/setup_env.sh
@@ -1,6 +1,4 @@
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 root_dir="$( cd "$( dirname "${script_dir}" )" &> /dev/null && pwd )"
 
-if [ ! -f "${root_dir}/defaults.yaml" ]; then
-    export COLCON_DEFAULTS_FILE="${root_dir}/defaults.yaml"
-fi
+export COLCON_DEFAULTS_FILE="${root_dir}/defaults.yaml"
