b6fd8806298dc4ae72ca96bc7c8a612e278efcaa
Turra <joao.t06@hotmail.com>
2024-11-03 19:54:49 -0300

    Ultrassonic Hw Protection layer

--- code/behaviors/pso_adaptative/pso_adaptative/hardware_protection_layer_us.py
+++ code/behaviors/pso_adaptative/pso_adaptative/hardware_protection_layer_us.py
@@ -3,17 +3,23 @@ import rclpy
 
 from functools import partial
 from rclpy.qos import qos_profile_sensor_data
-from std_msgs.msg import Float32
-from geometry_msgs.msg import Twist
+from std_msgs.msg import Float32, Bool
+from geometry_msgs.msg import Twist, Pose
 
 from ros2swarm.hardware_protection_layer import HardwareProtectionLayer
+from ros2swarm.utils.scan_calculation_functions import ScanCalculationFunctions
 from communication_interfaces.msg import RangeData
+from typing import Tuple
 
-
-FRONT = 0
-LEFT = 1
+LEFT = 0
+FRONT = 1
 RIGHT = 2
 
+# Position type
+PosType = Tuple[float, float]
+
+# Memory type
+MemType = Tuple[PosType, float]
 
 class HardwareProtectionLayerUS(HardwareProtectionLayer):
     def __init__(self):
@@ -22,120 +28,157 @@ class HardwareProtectionLayerUS(HardwareProtectionLayer):
         self.us_front_sub = self.create_subscription(
             Float32,
             self.get_namespace() + "/us_front",
-            self.swarm_command_controlled(
-                partial(self.ultrasonic_to_range, index=FRONT)
-            ),
+            self.swarm_command_controlled(partial(self.ultrasonic_to_range, index=FRONT)),
             qos_profile=qos_profile_sensor_data,
         )
 
         self.us_left_sub = self.create_subscription(
             Float32,
             self.get_namespace() + "/us_left",
-            self.swarm_command_controlled(
-                partial(self.ultrasonic_to_range, index=LEFT)
-            ),
+            self.swarm_command_controlled(partial(self.ultrasonic_to_range, index=LEFT)),
             qos_profile=qos_profile_sensor_data,
         )
 
         self.us_right_sub = self.create_subscription(
             Float32,
             self.get_namespace() + "/us_right",
-            self.swarm_command_controlled(
-                partial(self.ultrasonic_to_range, index=RIGHT)
-            ),
+            self.swarm_command_controlled(partial(self.ultrasonic_to_range, index=RIGHT)),
             qos_profile=qos_profile_sensor_data,
         )
 
-        self.current_angles = [0.0, -np.pi, np.pi]
-        self.current_ranges = [0.0, 0.0, 0.0]
-        self.angles = self.current_angles
+        self.range_pub = self.create_publisher(
+            RangeData,
+            self.get_namespace() + "/range_data",
+            qos_profile_sensor_data
+        )
 
+        self.has_obstacle_pub = self.create_publisher(
+            Bool,
+            self.get_namespace() + "/has_obstacle",
+            qos_profile_sensor_data
+        )
+
+        self.ranges = [0.0, 0.0, 0.0]
+        self.us_angles = [0.0, 1.5708, 3.14159]
+
+        self.requested_drive_command = Twist()
+
+        self.update_timer = self.create_timer(0.1, self.swarm_command_controlled_timer(self.update))
+
+    # PSO Adaptative functions
+    def initialize_aor_variables(self, k: float = 0.01, p: float = 0.9, trr: float = 0.1) -> None:
+        # theta -> Angle of rotation (AoR)
+        self.theta = 0 
+        # s -> Dynamic change step for AoR
+        self.s = 1 if np.random.rand() > 0.5 else -1
+        # dc -> Probability of changing s each interaction
+        self.dc = 0
+        # temperature -> Prevent theta decreasing too quickly
+        self.temperature = 0
+        # Trr -> Temperature reduction rate
+        self.Trr = trr
+
+        # Constants
+        self.k = k
+        self.p = p
+
+    def initialize_mem_tool(self, mms: int = 10, mpr: float = 0.1, md: float = 0.1, mpp1: float = 0.1, mpp2: float = 0.1, FFthr: float = 0.0) -> None:
+        self.mem: list[MemType] = []
+        self.mms = mms # Maximum Memory Size
+        self.mpr = mpr # Memory Point Radius
+        self.md = md # Minimum Displacement
+        self.mpp1 = mpp1 # Memory Point Penalty 1
+        self.mpp2 = mpp2 # Memory Point Penalty 2
+        self.FFthr = FFthr # Threshold
+
+    def update_theta(self, direction: Twist) -> np.ndarray:
+        V = [direction.linear.x, direction.angular.z]
+        
+        # Update velocity using AoR concept [7]
+        RV = [[np.cos(self.theta), -np.sin(self.theta)], [np.sin(self.theta), np.cos(self.theta)]]
+        RV = np.matmul(RV, V)
+
+        # Update theta with temperature [8]
+        self.theta = self.temperature * self.theta
+
+        # Update temperature [9]
+        self.temperature = self.Trr * self.temperature
+
+        return RV
+
+    # HPL functions
     def ultrasonic_to_range(self, msg: Float32, index: int):
-        self.current_ranges[index] = msg.data
+        self.ranges[index] = msg.data
 
         ranges: RangeData = RangeData()
-        ranges.ranges = self.current_ranges
-        ranges.angles = self.current_angles
+        ranges.header.stamp = self.get_clock().now().to_msg()
+        ranges.header.frame_id = self.get_namespace()
 
-        self.range_data_callback(ranges)
+        ranges.angles = self.us_angles
+        ranges.ranges = self.ranges
 
-    def command_callback(self, msg: Twist):
-        self.get_logger().debug('heard: "%s"' % msg)
+        self.range_pub.publish(ranges)
 
-        [adjust, direction] = self.check_collision(msg)
-
-        if adjust:
-            msg = direction
-            self.get_logger().debug('Adjusting to"%s"' % direction)
-
-        msg = self.apply_limits_to_command(msg)
-
-        self.publisher_cmd_vel.publish(msg)
-
-    def apply_limits_to_command(self, msg: Twist):
-        if np.abs(msg.linear.x) > self.param_max_translational_velocity:
-            msg.linear.x = np.sign(msg.linear.x) * self.param_max_translational_velocity
-        if np.abs(msg.angular.z) > self.param_max_rotational_velocity:
-            msg.angular.z = np.sign(msg.angular.z) * self.param_max_rotational_velocity
-
-        return msg
-
-    def check_collision(self, msg: Twist):
+    def vector_calc(self):
         if self.current_ranges is None:
+            print("[DEBUG] No range data received")
             return [False, None]
 
-        avoid_needed = False
-        direction = msg
-
-        stop_distance = self.param_min_range
         avoid_distance = self.param_max_range
+        direction, obstacle_free = ScanCalculationFunctions.potential_field(self.param_front_attraction,
+                                                     avoid_distance,
+                                                     self.param_max_rotational_velocity,
+                                                     self.param_max_translational_velocity,
+                                                     self.param_min_range,
+                                                     self.param_threshold,
+                                                     self.current_ranges,
+                                                     self.angles)
 
-        # Frontal collision
-        if (
-            self.current_ranges[FRONT] < stop_distance
-            and self.current_ranges[FRONT] > 0
-        ):
-            self.get_logger().warning("Front Collision detected")
-            direction.linear.x = 0.0
-            avoid_needed = True
-        # Left collision
-        if self.current_ranges[LEFT] < stop_distance and self.current_ranges[LEFT] > 0:
-            self.get_logger().warning("Left Collision detected")
-            # I need to turn slowly to right ( negative )
-            direction.angular.z -= 0.01 * self.param_max_rotational_velocity
-            avoid_needed = True
-        # Right collision
-        if (
-            self.current_ranges[RIGHT] < stop_distance
-            and self.current_ranges[RIGHT] > 0
-        ):
-            self.get_logger().warning("Right Collision detected")
-            # I need to turn slowly to left ( positive )
-            direction.angular.z += 0.01 * self.param_max_rotational_velocity
-            avoid_needed = True
 
-        self.get_logger().info(f"Distances: {self.current_ranges}")
+        avoid_needed = not obstacle_free
+        direction = self.wrap_direction(direction)
 
         return [avoid_needed, direction]
+    
+    def wrap_direction(self, direction: Twist):
+        direction.linear.x = np.clip(direction.linear.x, -self.param_max_translational_velocity, self.param_max_translational_velocity)
+        direction.angular.z = np.clip(direction.angular.z, -self.param_max_rotational_velocity, self.param_max_rotational_velocity)
+        return direction
 
-        # avoid_distance = self.param_max_range
-        # direction, obstacle_free = ScanCalculationFunctions.potential_field(self.param_front_attraction,
-        #                                              avoid_distance,
-        #                                              self.param_max_rotational_velocity,
-        #                                              self.param_max_translational_velocity,
-        #                                              self.param_min_range,
-        #                                              self.param_threshold,
-        #                                              self.current_ranges,
-        #                                              self.angles)
+    def command_callback(self, msg):
+        self.get_logger().debug('heard: "%s"' % msg)
 
+        self.requested_drive_command = msg
+
+    def range_data_callback(self, msg):
+        self.get_logger().debug('heard: "%s"' % msg)
+        self.current_ranges = msg.ranges
+        self.angles = msg.angles      
+
+    def update(self):
+        avoid_distance = self.param_max_range
+        direction = self.requested_drive_command
+
+        ranges = ScanCalculationFunctions.adjust_ranges(self.current_ranges, self.param_min_range, avoid_distance)
+        obstacle_free = ScanCalculationFunctions.is_obstacle_free(avoid_distance, ranges, self.param_threshold)
+
+        if not obstacle_free:
+            self.get_logger().info('Obstacle detected')
+        
+        self.has_obstacle_pub.publish(Bool(data=(not obstacle_free)))
+
+        direction = self.wrap_direction(direction)
+        self.publisher_cmd_vel.publish(direction)
 
 def main(args=None):
     rclpy.init(args=args)
+
     node = HardwareProtectionLayerUS()
     rclpy.spin(node)
     node.destroy_node()
+
     rclpy.shutdown()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
